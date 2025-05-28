4b044bd181612d44f55adb9acb9e9d552f20e3cc
mhneloy <mhneloy708@gmail.com>
2025-05-28 13:04:23 +0600

    changes the card design

--- src/component/RunningMarathonCard/RunningMarathonCard.jsx
+++ src/component/RunningMarathonCard/RunningMarathonCard.jsx
@@ -13,43 +13,32 @@ const RunningMarathonCard = ({ marathon }) => {
   } = marathon;
 
   return (
-    <motion.div
-      className="card bg-base-100 shadow-xl w-full border border-gray-200"
-      whileHover={{
-        scale: [1, 0.9, 1],
-      }}
-      transition={{
-        duration: 2,
-        repeat: Infinity,
-      }}
-    >
+    <div className=" relative rounded-md overflow-hidden group">
       {/* Image Section */}
       <figure className="relative">
         <img
           src={marathonImage}
           alt={title}
-          className="w-full h-48 object-cover"
+          className="w-full h-96 object-cover"
         />
         <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-t from-black to-transparent opacity-50"></div>
       </figure>
 
       {/* Card Body */}
-      <div className="card-body">
+      <div className="p-2 card-body absolute -bottom-1/2 left-0 w-full h-1/2 group-hover:bottom-0 group-hover:bg-gray-800 group-hover:bg-opacity-50 group-hover:transition-all text-gray-900">
         {/* Marathon Title */}
-        <h2 className="card-title text-lg font-semibold text-gray-800">
-          {title}
-        </h2>
+        <h2 className="card-title text-lg font-semibold text-white">{title}</h2>
 
         {/* Location */}
-        <div className="flex items-center text-sm text-gray-600 mt-2">
+        <div className="flex items-center text-sm text-white">
           <FaMapMarkerAlt className="mr-2 text-primary" />
           {location}
         </div>
 
         {/* Registration Dates */}
-        <p className="text-sm text-gray-500 mt-2">
+        <p className="text-sm text-white mt-2">
           Registration:{" "}
-          <span className="font-medium text-gray-700">
+          <span className="font-medium text-white">
             {startRegistrationDate} - {endRegistrationDate}
           </span>
         </p>
@@ -61,7 +50,7 @@ const RunningMarathonCard = ({ marathon }) => {
           </Link>
         </div>
       </div>
-    </motion.div>
+    </div>
   );
 };
 
